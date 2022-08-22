b088924f55c0ca9a6f9304f9b6a0b400ff62b77d
Andre Espinoza <ae95caba@gmail.com>
2022-08-22 09:18:58 -0300

    perfect

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var playerselection 
 
+
+
+
 //returns a random number between 0 an 2 which will be used to decid between rock paper or scisors
+
+
 function randomNumber(){
    return Math.floor(Math.random()*3);
 }
@@ -8,12 +13,13 @@ function randomNumber(){
 //uses the randomNumber function to make the computer pick rock paper or scissors
 
 function getComputerChoice(){
-   if (randomNumber()===0) {
+   var rdmNum = randomNumber();
+   if (rdmNum===0) {
       var choice="Scissors";
-   }else if (randomNumber()===1){
+   }else if (rdmNum===1){
          var choice="Paper";
       }
-   else {
+   else if (rdmNum===2){
       var choice="Rock";
    };
    return choice
@@ -21,7 +27,8 @@ function getComputerChoice(){
 
 
 //plays 1 round of rock paper scissors returning the winner as a string
-
+//in the computerselection parameter, the function getComputerChoice() should be used
+//the first parameter makes it so the defaul value will be to show a prompt whos input will be the  playerselection
 
 function playRound(playerselection= prompt("select rock paper or scissors").toLowerCase(),computerselection){
   
@@ -70,37 +77,47 @@ function playRound(playerselection= prompt("select rock paper or scissors").toLo
  
 
 
-//every time the game is run in the loop, a counter will increase every time someone wins
+
 function game(){
+   //every time the game is run in the loop, a counter will increase every time someone wins
    var playerWinsCount = 0;
    var computerWinsCount = 0;
 
    for (var i = 1; i < 6; i++) {
-      var roundResult = console.log( playRound(playerselection,getComputerChoice()));
-      console.log (i);
-      
+      //this var is created so the function playRound will only run once, there
+      var roundResult = (playRound(playerselection,getComputerChoice()));
+        //this will show in console who wins each round
+      console.log(roundResult);
+      //this will show in console the round number
+      console.log (`Round ${i}`);
+      //this is just a counter
       if (roundResult==="Player wins"){
         playerWinsCount++;
       }else if (roundResult==="Player looses"){
          computerWinsCount++;
       }
+      //this shows in console the wins counter
+      console.log(`Player wins:${playerWinsCount}`);
+      console.log(`Cimputer wins: ${computerWinsCount}`);
 
    };
 
-   //shows the final result
+   //this RETURNS the final result based of the previous counter
    
+
    if (playerWinsCount>computerWinsCount){
+      
       return "Player Wins match";
    }else if (computerWinsCount>playerWinsCount){
       return "Computer Wins match";   
-   }else {
+   }else if(computerWinsCount===playerWinsCount){
       return "Is a tie match";
-   }
+   };
 
 }  
 
 
-
+//this SHOWS in the console the final result, plus every console log inside game()
 console.log(game());
 
 
